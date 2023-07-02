c7ecd57aa102ab62b0d2b6db10e9e10b5fc09b9c
Ilze Rigerte <irigerte@gmail.com>
2023-07-02 18:13:40 +0300

    nav bar, user dash, bootstrap set, login, update, refresh, interceptors

---
+++ src/app/service/user.service.ts
@@ -0,0 +1,61 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, tap, throwError } from 'rxjs';
+import { CustomHttpResponse, Profile } from '../interface/appstates';
+import { User } from '../interface/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private readonly server: string = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) {}
+
+  login$ = (email: string, password: string) =>
+    <Observable<CustomHttpResponse<Profile>>>this.http
+      .post<CustomHttpResponse<Profile>>(`${this.server}/user/login`, {
+        email,
+        password,
+      })
+      .pipe(tap(console.log), catchError(this.handleError));
+
+  verifyCode$ = (email: string, code: string) =>
+    <Observable<CustomHttpResponse<Profile>>>(
+      this.http
+        .get<CustomHttpResponse<Profile>>(
+          `${this.server}/user/verify/code/${email}/${code}`
+        )
+        .pipe(tap(console.log), catchError(this.handleError))
+    );
+
+  profile$ = () =>
+    <Observable<CustomHttpResponse<Profile>>>(
+      this.http
+        .get<CustomHttpResponse<Profile>>(`${this.server}/user/profile`)
+        .pipe(tap(console.log), catchError(this.handleError))
+    );
+
+  update$ = (user: User) =>
+    <Observable<CustomHttpResponse<Profile>>>(
+      this.http
+        .patch<CustomHttpResponse<Profile>>(`${this.server}/user/update`, user)
+        .pipe(tap(console.log), catchError(this.handleError))
+    );
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    console.log(error);
+    let errorMessage: string;
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `A client error occurred - ${error.error.message}`;
+    } else {
+      if (error.error.reason) {
+        errorMessage = error.error.reason;
+        console.log(errorMessage);
+      } else {
+        errorMessage = `An error occurred - Error status ${error.status}`;
+      }
+    }
+    return throwError(() => errorMessage);
+  }
+}
