e0e56243b241c5a82c90fb3c4b54818e4a6ec51a
Ilze Rigerte <irigerte@gmail.com>
2023-07-05 17:28:59 +0300

    user activities display

--- src/app/component/profile/profile.component.ts
+++ src/app/component/profile/profile.component.ts
@@ -9,6 +9,7 @@ import {
   startWith,
 } from 'rxjs';
 import { DataState } from 'src/app/enum/datastate.enum';
+import { EventType } from 'src/app/enum/event-type.enum';
 import { CustomHttpResponse, Profile } from 'src/app/interface/appstates';
 import { State } from 'src/app/interface/state';
 import { UserService } from 'src/app/service/user.service';
@@ -23,7 +24,10 @@ export class ProfileComponent implements OnInit {
   private dataSubject = new BehaviorSubject<CustomHttpResponse<Profile>>(null);
   private isLoadingSubject = new BehaviorSubject<boolean>(false);
   isLoading$ = this.isLoadingSubject.asObservable();
+  private showLogsSubject = new BehaviorSubject<boolean>(false);
+  showLogs$ = this.showLogsSubject.asObservable();
   readonly DataState = DataState;
+  readonly EventType = EventType;
 
   constructor(private userService: UserService) {}
 
@@ -85,6 +89,7 @@ export class ProfileComponent implements OnInit {
         .pipe(
           map((response) => {
             console.log(response);
+            this.dataSubject.next({ ...response, data: response.data });
             passwordForm.reset();
             this.isLoadingSubject.next(false);
             return {
@@ -238,6 +243,11 @@ export class ProfileComponent implements OnInit {
         );
     }
   }
+
+  toggleLogs(): void {
+    this.showLogsSubject.next(!this.showLogsSubject.value);
+  }
+
   private getFormData(image: File): FormData {
     const formData = new FormData();
     formData.append('image', image);
