9155ce0cbaa83dec34fbe8b72203f6392be0887a
Ilze Rigerte <irigerte@gmail.com>
2023-11-15 17:30:00 +0200

    heroku configuration

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static("./dist/BetterStuffFrontEnd"));
+
+app.get('/*', (req, res) => {
+    res.sendFile("index.html", { root: "./dist/BetterStuffFrontEnd/" });
+});
+
+app.listen(process.env.PORT || 80);
