74dca87329a43a618b4190c88f4d9f24318c681b
Ilze Rigerte <irigerte@gmail.com>
2023-11-15 18:25:09 +0200

    server file

--- src/server (1).js
+++ src/server (1).js
@@ -5,8 +5,8 @@ const app = express();
 
 app.use(express.static("./dist/BetterStuffFrontEnd"));
 
-app.get('/*', (req, res) => {
-    res.sendFile("index.html", { root: "./dist/BetterStuffFrontEnd/" });
+app.get("/*", (req, res) => {
+  res.sendFile("index.html", { root: "./dist/BetterStuffFrontEnd/" });
 });
 
 app.listen(process.env.PORT || 80);
